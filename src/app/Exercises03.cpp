c398d75b8436269b79bd2075c9dcb81a59eed8f7
Phoenyx <trescojustin@gmail.com>
2023-02-07 10:06:22 -0500

    Created Exercises03.cpp

---
+++ src/app/Exercises03.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+#include <thread>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int, const char*);
+
+int main(int argc, char **argv){
+    std::cout<<"Begin OpenGL adventure"<<"\n";
+
+    glfwSetErrorCallback(error_callback);
+
+    if(!glfwInit()){
+        std::cerr<<"GLFW FAILED"<<"\n";
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+
+    int winWidth = 640;
+    int winHeight = 480;
+    GLFWwindow *window = glfwCreateWindow(winWidth, winHeight, "Amazing OpenGL Window", NULL, NULL);
+
+    if(!window){
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    int fwidth = 0;
+    int fheight = 0;
+
+    glClearColor(0.8, 0.0, 0.1, 1.0);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwGetFramebufferSize(window, &fwidth, &fheight);
+        glViewport(0, 0, fwidth, fheight);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        std::this_thread::sleep_for(std::chrono::milliseconds(15));
+    }
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
+
+static void error_callback(int error, const char* desc){
+    std::cerr<<"ERROR"<<error<<": "<<desc<<"\n";
+}
