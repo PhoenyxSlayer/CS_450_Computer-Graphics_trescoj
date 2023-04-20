7e92af864a17d4ddfda42d3787c5dbf28993dd63
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-04-20 11:53:54 -0400

    Added view and projection and some beginning shading

--- shaders/ProfExercises07/Basic.fs
+++ shaders/ProfExercises07/Basic.fs
@@ -2,6 +2,35 @@
 
 layout(location=0) out vec4 out_color;
 
+struct PointLight {
+    vec4 pos;
+    vec4 color;
+};
+
+uniform PointLight light;
+
+in vec4 interPos;
+in vec3 interNormal;
+
 void main() {
-    out_color = vec4(0.0, 0.0, 1.0, 1.0);
+
+    vec4 kd = vec4(0,0,1,1);
+
+    vec3 L = vec3(light.pos - interPos);
+    float dist = length(L);
+    L = normalize(L);
+
+    float at = 1.0/(dist*dist + 1.0);
+
+    vec3 N = normalize(interNormal);
+
+    float diffFactor = max(0.0, dot(N, L));
+
+    vec4 diffColor = kd*light.color*diffFactor;
+
+
+    //out_color = vec4(0.0, 0.0, 1.0, 1.0);
+    //out_color = vec4(at, at, at, 1.0);
+    //out_color = vec4(N, 1.0);
+    out_color = diffColor;
 }
--- src/app/ProfExercises07.cpp
+++ src/app/ProfExercises07.cpp
@@ -15,6 +15,11 @@ using namespace std;
 
 glm::vec2 lastMousePos;
 
+struct PointLight {
+    glm::vec4 pos = glm::vec4(0,0,0,1);
+    glm::vec4 color = glm::vec4(1,1,1,1);
+};
+
 vector<GLfloat> vertOnly = {
     -0.3f, -0.3f, 0.0f,
     +0.3f, -0.3f, 0.0f,
@@ -25,11 +30,12 @@ vector<GLfloat> vertOnly = {
     +0.3f, +0.3f, 0.0f,
 };
 
+float squareSize = 0.7f;
 vector<GLfloat> vertList = {
-    -0.3f, +0.3f, 0.0f,
-    +0.3f, +0.3f, 0.0f,
-    -0.3f, -0.3f, 0.0f,
-    +0.3f, -0.3f, 0.0f,
+    -squareSize, +squareSize, 0.0f, 0.0f, 0.0f, 1.0f,
+    +squareSize, +squareSize, 0.0f, 0.0f, 0.0f, 1.0f,
+    -squareSize, -squareSize, 0.0f, 0.0f, 0.0f, 1.0f,
+    +squareSize, -squareSize, 0.0f, 0.0f, 0.0f, 1.0f,
 };
 
 vector<GLuint> indexList = {
@@ -226,6 +232,16 @@ int main(int argc, char **argv) {
                                             "modelMat");
     cout << "modelMatLoc: " << modelMatLoc << endl;
 
+    GLint viewMatLoc = glGetUniformLocation(progID, "viewMat");
+    GLint projMatLoc = glGetUniformLocation(progID, "projMat");
+
+    GLint lightPosLoc = glGetUniformLocation(progID, "light.pos");
+    GLint lightColorLoc = glGetUniformLocation(progID, "light.color");
+
+    PointLight myLight;
+    myLight.pos = glm::vec4(0,0,0.25,1);
+    myLight.color = glm::vec4(1,1,1,1);
+
     // Setup VBO
     GLuint vbo;
     glGenBuffers(1, &vbo);
@@ -256,10 +272,15 @@ int main(int argc, char **argv) {
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0); // pos
+    glEnableVertexAttribArray(1); // normal
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, false, 
-                            3*sizeof(float), 0);
+                            6*sizeof(float), 0);                        
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 
+                            6*sizeof(float), ((void*)(3*sizeof(float))));
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -284,6 +305,20 @@ int main(int argc, char **argv) {
         glUseProgram(progID);
 
         glUniformMatrix4fv(modelMatLoc, 1, false, glm::value_ptr(modelMat));
+
+        glm::mat4 viewMat = glm::lookAt(glm::vec3(1,0,1), glm::vec3(0,0,0), glm::vec3(0,1,0));
+        glUniformMatrix4fv(viewMatLoc, 1, false, glm::value_ptr(viewMat));
+
+        float aspect = 1.0f;
+        if(fwidth > 0 && fheight > 0) {
+            aspect = ((float)fwidth)/((float)fheight);
+        }
+        glm::mat4 projMat = glm::perspective(glm::radians(90.0f), aspect, 0.01f, 50.0f);
+        glUniformMatrix4fv(projMatLoc, 1, false, glm::value_ptr(projMat));
+
+        glm::vec4 viewLightPos = viewMat*myLight.pos;
+        glUniform4fv(lightPosLoc, 1, glm::value_ptr(viewLightPos));
+        glUniform4fv(lightColorLoc, 1, glm::value_ptr(myLight.color));
 
         glBindVertexArray(vao);
         //glDrawArrays(GL_TRIANGLES, 0, vcnt);
